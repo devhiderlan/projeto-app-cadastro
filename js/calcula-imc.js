bd533a0084321bf516e95c34e6e1915a97730e85
devhiderlan <dutrahiderlan@gmail.com>
2022-08-05 22:17:49 -0300

    Corrigido erro na função validaAltura

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector('h1')
-titulo.textContent = 'Tabela de Pacientes'
+titulo.textContent = 'Cadastro de Pacientes'
 
 var pacientes = document.querySelectorAll('.paciente')
 
@@ -13,17 +13,17 @@ for (i = 0; i < pacientes.length; i++) {
   var altura = tdAltura.textContent
   var peso = tdPeso.textContent
 
-  var pesoEhValido = true
-  var alturaEhValida = true
+  var pesoEhValido = validaPeso(peso)
+  var alturaEhValida = validaAltura(altura)
 
-  if (peso <= 0 || peso >= 1000) {
+  if (!pesoEhValido) {
     console.log('Peso inválido')
     pesoEhValido = false
     tdImc.textContent = 'O peso é inválido'
     paciente.classList.add('paciente-invalido')
   }
 
-  if (altura <= 0 || altura >= 2.5) {
+  if (!alturaEhValida) {
     console.log('Altura inválida')
     alturaEhValida = false
     tdImc.textContent = 'A altura é inválida'
@@ -33,6 +33,22 @@ for (i = 0; i < pacientes.length; i++) {
   if (pesoEhValido && alturaEhValida) {
     imc = calculaImc(peso, altura)
     tdImc.textContent = imc
+  }
+}
+
+function validaPeso(peso) {
+  if (peso >= 0 && peso <= 500) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 2.5) {
+    return true
+  } else {
+    return false
   }
 }
 
