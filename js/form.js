445b109b86e1f9c88bfa38b2bb6a5d247167faa1
devhiderlan <dutrahiderlan@gmail.com>
2022-08-05 22:16:10 -0300

    Adicionado validação dos campos do form na função validaPaciente

--- js/form.js
+++ js/form.js
@@ -7,18 +7,40 @@ botaoAdicionar.addEventListener('click', function (event) {
 
   // Extraindo informações do paciente
   var paciente = obtemPacienteDoFormulario(form)
-  console.log(paciente)
+  // console.log(paciente)
 
   //Cria a tr e td do paciente
   var pacienteTr = montaTr(paciente)
 
+  var erros = validaPaciente(paciente)
+  console.log(erros)
+
+  if (erros.length > 0) {
+    exibeMensagensDeErro(erros)
+    return
+  }
+
   //Adicionando o paciente na tabela.
   var tabela = document.querySelector('#tabela-pacientes')
   tabela.appendChild(pacienteTr)
-  console.log(pacienteTr)
+  // console.log(pacienteTr)
 
   form.reset()
+
+  var mensagensErro = document.querySelector('#mensagens-erro')
+  mensagensErro.innerHTML = ''
 })
+
+function exibeMensagensDeErro(erros) {
+  var ul = document.querySelector('#mensagens-erro')
+  ul.innerHTML = ''
+
+  erros.forEach(function (erro) {
+    var li = document.createElement('li')
+    li.textContent = erro
+    ul.appendChild(li)
+  })
+}
 
 function obtemPacienteDoFormulario(form) {
   var paciente = {
@@ -36,20 +58,49 @@ function montaTr(paciente) {
 
   pacienteTr.classList.add('paciente')
 
-  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
-  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
-  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
-  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
-  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+  pacienteTr.appendChild(montaTd(paciente.nome, 'info-nome'))
+  pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'))
+  pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'))
+  pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gordura'))
+  pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'))
 
   return pacienteTr
 }
 
-function montaTd(dado,classe){
-  var td = document.createElement("td");
-  td.textContent = dado;
-  td.classList.add(classe);
+function montaTd(dado, classe) {
+  var td = document.createElement('td')
+  td.textContent = dado
+  td.classList.add(classe)
 
-  return td;
+  return td
 }
 
+function validaPaciente(paciente) {
+  var erros = []
+
+  if (!validaPeso(paciente.peso)) {
+    erros.push('Peso inválido!')
+  }
+
+  if (!validaAltura(paciente.altura)) {
+    erros.push('Altura inválida!')
+  }
+
+  if (paciente.nome.length == 0) {
+    erros.push('O campo nome deve ser preenchido!')
+  }
+
+  if (paciente.gordura.length == 0) {
+    erros.push('O campo % de gordura deve ser preenchido!')
+  }
+
+  if (paciente.peso.length == 0) {
+    erros.push('O campo peso deve ser preenchido!')
+  }
+
+  if (paciente.altura.length == 0) {
+    erros.push('O campo altura deve ser preenchido!')
+  }
+
+  return erros
+}
